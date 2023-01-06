018cdeda11005340e975a1116ec1f72a467eb4fc
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2023-01-06 14:13:29 +0530

    Update Reach a Number.cpp

--- Array and String/REACH A NUMBER/Reach a Number.cpp
+++ Array and String/REACH A NUMBER/Reach a Number.cpp
@@ -1,1 +1,27 @@
-n
+https://leetcode.com/problems/reach-a-number/description/
+
+https://www.geeksforgeeks.org/find-the-number-of-jumps-to-reach-x-in-the-number-line-from-zero/
+
+Note:- The minimum steps to target is same as the minimum steps to abs(target).
+
+On each negative jump I cancel twice the effect because ( one its own and on next jump I have to recross that same length )
+  
+
+// CODE
+  
+class Solution {
+public:
+    int reachNumber(int target) {
+        // answer is min n with value of n*(n+1)/2 greater then target and difference even
+        int answer = 0; int n = 1;
+        target = abs(target);
+        int value = 0;
+        while(value < target || (value-target)%2 == 1)
+        {
+            answer += 1;
+            value+=n;
+            n++;
+        }
+        return answer;
+    }
+};
